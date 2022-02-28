3fa8bea3d161d664bc8e466b06e86e2b1d22078a
ArturMarschenkulov <ArturMarschals@web.de>
2022-02-28 05:18:19 +0100

    changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
 
 
+int main()
+{
+    std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-auto main() -> int {
-    return 54536;
+    for (const std::string& word : msg)
+    {
+        std::cout << word << " ";
+    }
+    std::cout << std::endl;
 }
